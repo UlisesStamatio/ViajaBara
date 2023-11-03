f2e36ba0f17cb52b5221d6b40e3e4be9df498361
20203tn096 <20203tn096@utez.edu.mx>
2023-11-03 17:55:03 -0600

    [ADD] feature new bus

---
+++ Web/viajabara/src/modules/buses/adapters/bus.storage.js
@@ -0,0 +1,95 @@
+import axiosInstance from "../../../config/axios"
+import { statusValidator } from "../../../kernel/validators/status.validator";
+import router from "../../../router/index";
+const BASE_URL_BUS = "/api/buses"
+
+const Operations = {
+    async newBus(url, data){
+      let response;
+      try {
+        response= await axiosInstance.post(`${BASE_URL_BUS}${url}`, JSON.stringify(data));
+        response = statusValidator(response);
+      } catch (error) {
+        if(error.response){
+          response = statusValidator(error.response);
+        }else{
+          router.push({name: 'Error Error403'})
+        }
+      }
+      return response;
+    },
+
+    // async lisUsers(url){
+    //   let response;
+    //   try {
+    //     response= await axiosInstance.get(`${BASE_URL_USER}${url}`);
+    //     response = statusValidator(response);
+    //   } catch (error) {
+    //     if(error.response){
+    //       response = statusValidator(error.response);
+    //     }else{
+    //       router.push({name: 'Error Error403'})
+    //     }
+    //   }
+    //   return response;
+    // },
+
+    // async listConsumers(url){
+    //   let response;
+    //   try {
+    //     response= await axiosInstance.get(`${BASE_URL_USER}${url}`);
+    //     response = statusValidator(response);
+    //   } catch (error) {
+    //     if(error.response){
+    //       response = statusValidator(error.response);
+    //     }else{
+    //       router.push({name: 'Error Error403'})
+    //     }
+    //   }
+    //   return response;
+    // },
+    // async getUser(url, data){
+    //   let response;
+    //   try {
+    //     response= await axiosInstance.put(`${BASE_URL_USER}${url}`, JSON.stringify(data));
+    //     response = statusValidator(response);
+    //   } catch (error) {
+    //     if(error.response){
+    //       response = statusValidator(error.response);
+    //     }else{
+    //       router.push({name: 'Error Error403'})
+    //     }
+    //   }
+    //   return response;
+    // },
+    // async updateUser(url, data){
+    //   let response;
+    //   try {
+    //     response= await axiosInstance.put(`${BASE_URL_USER}${url}`, JSON.stringify(data));
+    //     response = statusValidator(response);
+    //   } catch (error) {
+    //     if(error.response){
+    //       response = statusValidator(error.response);
+    //     }else{
+    //       router.push({name: 'Error Error403'})
+    //     }
+    //   }
+    //   return response;
+    // },
+    // async changeStatusUser(url, data){
+    //   let response;
+    //   try {
+    //     response= await axiosInstance.put(`${BASE_URL_USER}${url}`, JSON.stringify(data));
+    //     response = statusValidator(response);
+    //   } catch (error) {
+    //     if(error.response){
+    //       response = statusValidator(error.response);
+    //     }else{
+    //       router.push({name: 'Error Error403'})
+    //     }
+    //   }
+    //   return response;
+    // },
+}
+
+export default Operations;
