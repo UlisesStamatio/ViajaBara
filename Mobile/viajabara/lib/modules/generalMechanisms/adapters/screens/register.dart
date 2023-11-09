15ca38f50f5286dff02d3c14605844b35c5be499
Nahum Torres <20203TN058@utez.edu.mx>
2023-11-09 11:52:22 -0600

    [ADD] add register user

--- Mobile/viajabara/lib/modules/generalMechanisms/adapters/screens/register.dart
+++ Mobile/viajabara/lib/modules/generalMechanisms/adapters/screens/register.dart
@@ -15,6 +15,7 @@ class _RegisterState extends State<Register> {
   bool _passwordVisible = false;
   bool _repeatPasswordVisible = false;
   final TextEditingController _email = TextEditingController(text: '');
+  final TextEditingController _username = TextEditingController(text: '');
   final TextEditingController _pass = TextEditingController(text: '');
   final TextEditingController _repeatPass = TextEditingController(text: '');
 
@@ -37,7 +38,7 @@ class _RegisterState extends State<Register> {
                 fit: BoxFit.cover,
               ),
               Positioned(
-                top: 120,
+                top: 110,
                 left: 0,
                 right: 0,
                 child: Center(
@@ -56,7 +57,7 @@ class _RegisterState extends State<Register> {
                           children: <Widget>[
                             Container(
                               padding:
-                                  const EdgeInsets.only(bottom: 40, top: 20),
+                                  const EdgeInsets.only(bottom: 40, top: 10),
                               child: const Text(
                                 'Únete y viaja de volada',
                                 style: TextStyle(
@@ -86,7 +87,7 @@ class _RegisterState extends State<Register> {
                                   color: ColorsApp.text,
                                 ),
                                 decoration: InputDecoration(
-                                  labelText: 'Usuario o correo*',
+                                  labelText: 'Correo eléctronico*',
                                   hintText: "example.com",
                                   filled: true,
                                   fillColor: ColorsApp.whiteColor,
@@ -127,7 +128,65 @@ class _RegisterState extends State<Register> {
                             ),
                             Container(
                               padding: const EdgeInsets.only(
-                                bottom: 20,
+                                bottom: 10,
+                              ),
+                              child: TextFormField(
+                                keyboardType: TextInputType.name,
+                                controller: _username,
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Campo obligatorio';
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                                cursorColor: ColorsApp.primayColor,
+                                style: const TextStyle(
+                                  color: ColorsApp.text,
+                                ),
+                                decoration: InputDecoration(
+                                  labelText: 'Usuario*',
+                                  hintText: "Usurio96",
+                                  filled: true,
+                                  fillColor: ColorsApp.whiteColor,
+                                  hintStyle: const TextStyle(
+                                    color: ColorsApp.muted,
+                                  ),
+                                  labelStyle: const TextStyle(
+                                    color: ColorsApp.muted,
+                                  ),
+                                  prefixIcon: const Icon(Icons.person),
+                                  prefixIconColor: ColorsApp.primayColor,
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4.0),
+                                      borderSide: const BorderSide(
+                                          color: ColorsApp.muted,
+                                          width: 1.0,
+                                          style: BorderStyle.solid)),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4.0),
+                                      borderSide: const BorderSide(
+                                          color: ColorsApp.primayColor,
+                                          width: 1.0,
+                                          style: BorderStyle.solid)),
+                                  errorBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4.0),
+                                      borderSide: const BorderSide(
+                                          color: ColorsApp.errorColor,
+                                          width: 1.0,
+                                          style: BorderStyle.solid)),
+                                  focusedErrorBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4.0),
+                                      borderSide: const BorderSide(
+                                          color: ColorsApp.muted,
+                                          width: 1.0,
+                                          style: BorderStyle.solid)),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.only(
+                                bottom: 10,
                               ),
                               child: TextFormField(
                                 keyboardType: TextInputType.text,
@@ -211,7 +270,7 @@ class _RegisterState extends State<Register> {
                             ),
                             Container(
                               padding: const EdgeInsets.only(
-                                bottom: 20,
+                                bottom: 10,
                               ),
                               child: TextFormField(
                                 keyboardType: TextInputType.text,
@@ -303,7 +362,12 @@ class _RegisterState extends State<Register> {
                                 onPressed: _isButtonDisabled
                                     ? null
                                     : () => Navigator.pushReplacementNamed(
-                                        context, '/register2'),
+                                            context, '/register2',
+                                            arguments: {
+                                              'email': _email.text,
+                                              'password': _pass.text,
+                                              'username': _username.text,
+                                            }),
                                 style: ButtonStyle(
                                   backgroundColor: MaterialStateProperty.all(
                                     _isButtonDisabled
--- Mobile/viajabara/lib/providers/auth_provider.dart
+++ Mobile/viajabara/lib/providers/auth_provider.dart
@@ -1,15 +1,14 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+import 'package:viajabara/domain/entities/user_data.dart';
 
 class AuthProvider {
   Dio dio = Dio();
-  final url = 'http://192.168.0.101:8083/api/auth/login';
 
- Future<bool> login(String email, String password) async {
+Future<bool> login(String email, String password) async {
     var dataJson = jsonEncode({'email': email, 'password': password});
     try {
-      final response = await dio.post( url, data: dataJson );
+      final response = await dio.post( 'http://192.168.0.104:8083/api/auth/login', data: dataJson );
 
       return response.statusCode == 200;
     } on DioException catch (e) {
@@ -19,4 +18,41 @@ class AuthProvider {
       throw Exception('Error de inicio de sesión: ${e.message}');
     }
   }
+
+  Future<bool> register(UserData userData) async {
+    var dataJson = jsonEncode({
+      'profile': "Pruebaaaa",
+      'username': userData.username,
+      'email': userData.email,
+      'password': userData.password,
+      'person':{
+        'name': userData.name,
+        'surname': userData.surname,  
+        'cellphone': userData.cellphone,
+        'birthDate': userData.birthDate,
+        'sex': userData.sex,
+        'state': {
+          'id': 5
+        },
+      },
+      'roles': [
+        {
+          'id': 3
+        }
+      ]
+    });
+
+    try {
+      final response = await dio.post('http://192.168.0.104:8083/api/auth/register', data: dataJson);
+      print("Si entra aquí");
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } on DioException catch (e) {
+      throw Exception('Error al registrar al usuario: ${e.message}');
+    }
+  }
+
 }
