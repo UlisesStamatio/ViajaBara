aa58e98e1fc9e801e64bc111274f52f93d59b5e2
UlisesStamatio <20203tn159@utez.edu.mx>
2023-12-11 18:54:51 -0600

    [ADD] ClientHistory

--- Back/src/main/java/mx/edu/utez/viajabara/generalmethods/control/ClientController.java
+++ Back/src/main/java/mx/edu/utez/viajabara/generalmethods/control/ClientController.java
@@ -1,10 +1,14 @@
 package mx.edu.utez.viajabara.generalmethods.control;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import mx.edu.utez.viajabara.basecatalog.seatingSales.control.SeatingSalesService;
+import mx.edu.utez.viajabara.basecatalog.seatingSales.model.SeatingSalesDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/client")
@@ -12,4 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = {"*"}, methods = { RequestMethod.GET,RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ClientController {
 
+    private final SeatingSalesService service;
+
+    @Autowired
+    public ClientController(SeatingSalesService service) {
+        this.service = service;
+    }
+
+    @PutMapping("/all-by-client")
+    @ApiOperation(
+            value = "Obtiene todas las ventas de asientos por un usuario",
+            notes = "{\n" +
+                    "    \"client\": {\n" +
+                    "        \"id\": 1\n" +
+                    "    }\n" +
+                    "}"
+    )
+    public ResponseEntity<Object> findAllByClient(@Validated(SeatingSalesDto.FindByClient.class) @RequestBody SeatingSalesDto dto) {
+        return service.findAllByClient(dto);
+    }
 }
